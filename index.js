3542f57f55c8f3508870dab7fdc5d08620101129
riskieee <git@riskieee.de>
2021-05-03 20:32:39 +0200

    rob_wip_consoleinfo

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('Stop Using console.log() In JavaScript');
+console.warn('There is more to explore');
+console.log('https://javascript.plainenglish.io/stop-using-console-log-in-javascript-d29d6c24dc26');
