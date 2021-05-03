7d34e34cd37953cbb661cef147aff0f723759473
riskieee <git@riskieee.de>
2021-05-03 21:04:22 +0200

    Add small text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.group('infos')
 console.error('Stop Using console.log() In JavaScript');
-console.warn('There is more to explore');
+console.warn('There is really more to explore');
 console.log('https://javascript.plainenglish.io/stop-using-console-log-in-javascript-d29d6c24dc26');
 console.log('https://javascript.plainenglish.io/stop-using-console-log-in-javascript-d29d6c24dc26');
